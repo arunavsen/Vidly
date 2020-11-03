00096faa1b1d6029b8d0093347e4aa4380992d8a
Arunav <arunav@vonome.com>
2020-11-03 19:49:32 +0600

    Movie, Customer, RandomMovieCustomerViewModel, MoviesController, RandomView LUX theme created

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Patalok"
+            };
+
+            var customers = new List<Customer>()
+            {
+                new Customer{Name = "Customer 1"},
+                new Customer{Name = "Customer 2"}
+            };
+
+            var VM = new RandomCustomerMovieViewModel()
+            {
+                Customers = customers,
+                Movie = movie
+            };
+
+            return View(VM);
+        }
+    }
+}
