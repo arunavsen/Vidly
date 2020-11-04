68271ba12923835311fb521acd66bff3099aed9f
Arunav <arunav@vonome.com>
2020-11-04 23:06:57 +0600

    Add MembershipType Class, Change view in Index

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,25 +11,28 @@ namespace Vidly.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    _context.Dispose();
+        //}
         // GET: Customer
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(m =>m.MembershipType).ToList();
             return View(customers);
         }
 
-        private List<Customer> GetCustomers()
-        {
-            return new List<Customer>()
-            {
-                new Customer{Id = 1, Name = "Will Smith"},
-                new Customer{Id = 2, Name = "Jack Jones"}
-            };
-        }
-
+        
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
             {
                 return HttpNotFound();
