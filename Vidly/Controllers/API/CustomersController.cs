2e7cf1721c43b0f31bbbe5b433d669344a0a0218
Arunav <arunav@vonome.com>
2020-11-11 14:31:15 +0600

    Exercise 5 solution

---
+++ Vidly/Controllers/API/CustomersController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using AutoMapper;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.API
+{
+    public class CustomersController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/customers
+        public IHttpActionResult GetCustomers()
+        {
+            var customerDtos = _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
+
+            return Ok(customerDtos);
+        }
+
+        // GET /api/customers/1
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(m => m.Id == id);
+            if (customerInDb == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(Mapper.Map<Customer, CustomerDto>(customerInDb));
+            }
+        }
+
+        // POST /api/customers
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            customerDto.Id = customer.Id;
+            return Created(new Uri(Request.RequestUri+"/"+customer.Id), customerDto);
+        }
+
+        // PUT /api/customers/1
+        [System.Web.Http.HttpPut]
+        public IHttpActionResult UpdateCustomer(int id, CustomerDto customerDto)
+        {
+            if (Equals(!ModelState.IsValid))
+            {
+                return BadRequest();
+            }
+
+            var customerInDb = _context.Customers.SingleOrDefault(m => m.Id == id);
+
+            if (customerInDb == null)
+            {
+                return NotFound();
+            }
+
+            Mapper.Map(customerDto, customerInDb);
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        // DELETE /api/customers
+        [System.Web.Http.HttpDelete]
+        public IHttpActionResult DeleteCustomers(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(m => m.Id == id);
+            if (customerInDb == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
